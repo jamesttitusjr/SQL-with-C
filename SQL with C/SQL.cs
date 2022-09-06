f1583a3764d9c3d8bbb2aca92a321d66852d4e04
James Titus <jamesttitusjr@outlook.com>
2022-09-06 09:19:39 -0400

    Add project files.

---
+++ SQL with C/SQL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_with_C
+{
+    internal class SQL
+    {
+    }
+}
+
+/* server=localhost\\sqlexpress;databases
